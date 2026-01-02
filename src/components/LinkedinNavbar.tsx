e954f375ad04c219a634edc8390f78c44c3418ff
achrekarom12 <88442486+achrekarom12@users.noreply.github.com>
2026-01-02 17:08:54 +0530

    fix: me section in navbar on mobile device

--- src/components/LinkedinNavbar.tsx
+++ src/components/LinkedinNavbar.tsx
@@ -34,8 +34,8 @@ export default function LinkedinNavbar() {
 
     return (
         <header className="fixed top-0 left-0 w-full bg-[var(--nav-bg)] border-b border-[var(--nav-border)] z-50 h-[56px] transition-colors duration-200">
-            <div className="container-max h-full flex items-center justify-between px-4 sm:px-0 relative">
-                <div className="flex items-center gap-2 flex-1 md:flex-none">
+            <div className="container-max h-full flex items-center justify-between px-2 sm:px-0 relative">
+                <div className="flex items-center gap-2 md:flex-none">
                     <Link href="/">
                         <svg viewBox="0 0 24 24" className="w-[34px] h-[34px] fill-[#0a66c2]" aria-label="LinkedIn">
                             <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.22-.44-2.12-1.61-2.12a1.77 1.77 0 00-1.63 1.15.86.86 0 00-.06.42V19h-3v-9h2.9v1.3a2.89 2.89 0 012.63-1.45c1.92 0 3.37 1.28 3.37 4.02z"></path>
@@ -67,8 +67,8 @@ export default function LinkedinNavbar() {
                 </div>
 
                 {/* Navigation - Desktop and Mobile (Custom Icons) */}
-                <nav className="flex items-center h-full space-x-0 md:space-x-1 pt-0 md:pt-2">
-                    <Link href="/" className="flex flex-col items-center justify-center min-w-[48px] md:min-w-[80px] h-[52px] md:border-b-2 border-[var(--text-main)] text-[var(--text-main)] transition-colors">
+                <nav className="flex flex-1 justify-end items-center h-full space-x-0 md:space-x-1 pt-0 md:pt-2">
+                    <Link href="/" className="flex flex-col items-center justify-center min-w-[40px] md:min-w-[80px] h-[52px] md:border-b-2 border-[var(--text-main)] text-[var(--text-main)] transition-colors">
                         <div className="relative">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24" focusable="false"><path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7z"></path></svg>
                             <span className="absolute -top-1 -right-1 bg-[#cc1016] border-2 border-[var(--nav-bg)] rounded-full h-3 w-3"></span>
@@ -76,22 +76,22 @@ export default function LinkedinNavbar() {
                         <span className="text-[12px] mt-0.5 hidden md:block">Home</span>
                     </Link>
 
-                    <Link href="#" className="flex flex-col items-center justify-center min-w-[48px] md:min-w-[80px] h-[52px] text-[var(--text-dim)] hover:text-[var(--text-main)] transition-colors">
+                    <Link href="#" className="flex flex-col items-center justify-center min-w-[40px] md:min-w-[80px] h-[52px] text-[var(--text-dim)] hover:text-[var(--text-main)] transition-colors">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24" focusable="false"><path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"></path></svg>
                         <span className="text-[12px] mt-0.5 hidden md:block">My Network</span>
                     </Link>
 
-                    <Link href="#" className="flex flex-col items-center justify-center min-w-[48px] md:min-w-[80px] h-[52px] text-[var(--text-dim)] hover:text-[var(--text-main)] transition-colors">
+                    <Link href="#" className="flex flex-col items-center justify-center min-w-[40px] md:min-w-[80px] h-[52px] text-[var(--text-dim)] hover:text-[var(--text-main)] transition-colors">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24" focusable="false"><path d="M17 6V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2v4a3 3 0 003 3h14a3 3 0 003-3V6zM9 5a1 1 0 011-1h4a1 1 0 011 1v1H9zm10 9a4 4 0 003-1.38V17a3 3 0 01-3 3H5a3 3 0 01-3-3v-4.38A4 4 0 005 14z"></path></svg>
                         <span className="text-[12px] mt-0.5 hidden md:block">Jobs</span>
                     </Link>
 
-                    <Link href="#" className="flex flex-col items-center justify-center min-w-[48px] md:min-w-[80px] h-[52px] text-[var(--text-dim)] hover:text-[var(--text-main)] transition-colors">
+                    <Link href="#" className="flex flex-col items-center justify-center min-w-[40px] md:min-w-[80px] h-[52px] text-[var(--text-dim)] hover:text-[var(--text-main)] transition-colors">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24" focusable="false"><path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path></svg>
                         <span className="text-[12px] mt-0.5 hidden md:block">Messaging</span>
                     </Link>
 
-                    <Link href="#" className="flex flex-col items-center justify-center min-w-[48px] md:min-w-[80px] h-[52px] text-[var(--text-dim)] hover:text-[var(--text-main)] transition-colors">
+                    <Link href="#" className="flex flex-col items-center justify-center min-w-[40px] md:min-w-[80px] h-[52px] text-[var(--text-dim)] hover:text-[var(--text-main)] transition-colors">
                         <div className="relative">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24" focusable="false"><path d="M22 19h-8.28a2 2 0 11-3.44 0H2v-1a4.52 4.52 0 011.17-2.83l1-1.17h15.7l1 1.17A4.42 4.42 0 0122 18zM18.21 7.44A6.27 6.27 0 0012 2a6.27 6.27 0 00-6.21 5.44L5 13h14z"></path></svg>
                             <span className="absolute -top-1 -right-1 bg-[#cc1016] text-white text-[10px] font-bold rounded-full h-4 w-4 flex items-center justify-center border-2 border-[var(--nav-bg)]">1</span>
@@ -102,12 +102,12 @@ export default function LinkedinNavbar() {
                     <div className="relative" ref={dropdownRef}>
                         <button
                             onClick={() => setIsMeOpen(!isMeOpen)}
-                            className="hidden md:flex flex-col items-center justify-center cursor-pointer min-w-[80px] h-[52px] border-b-2 border-transparent text-[var(--text-dim)] hover:text-[var(--text-main)] ml-1 border-l border-[var(--divider)] pl-2 transition-colors duration-200"
+                            className="flex flex-col items-center justify-center cursor-pointer min-w-[40px] md:min-w-[80px] h-[52px] border-b-2 border-transparent text-[var(--text-dim)] hover:text-[var(--text-main)] md:ml-1 md:border-l border-[var(--divider)] md:pl-2 transition-colors duration-200"
                         >
-                            <div className="w-6 h-6 rounded-full bg-gray-300 overflow-hidden">
+                            <div className="w-6 h-6 rounded-full bg-gray-300 overflow-hidden shrink-0">
                                 <img src="/my_image.png" alt="Me" className="w-full h-full object-cover" />
                             </div>
-                            <div className="flex items-center mt-0.5">
+                            <div className="hidden md:flex items-center mt-0.5">
                                 <span className="text-[12px]">Me</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" width="16" height="16" focusable="false" className={`transition-transform duration-200 ${isMeOpen ? 'rotate-180' : ''}`}><path d="M8 11L3 6h10z" fillRule="evenodd"></path></svg>
                             </div>
